1ec51cfd73d97777e38b53698b4212b7607e366e
m1994d <marlondiaz2994@gmail.com>
2022-09-01 11:10:50 -0500

    fixing code monty

---
+++ freeStack.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * freeStack - It frees the entire stack
+ * @stack: stack where this function will operate
+ *
+ *
+ * Return: nothing
+ * On error: nothing
+ */
+
+void freeStack(stack_t **stack)
+{
+	stack_t *current = *stack;
+
+	for (; current; current = *stack)
+	{
+		*stack = current->next;
+		free(current);
+	}
+}
