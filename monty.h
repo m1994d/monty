bf1cf843a868d3ac086966b2c45bf5a946fe9a3f
m1994d <marlondiaz2994@gmail.com>
2022-09-01 11:28:57 -0500

    fixing code

--- monty.h
+++ monty.h
@@ -59,26 +59,10 @@ struct pack
 } pack;
 
 void freeStack(stack_t **stack);
-int isNumber(char *str);
-char *str_concat(char *s1, char *s2);
+
 void built_in(stack_t **head);
 void push(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void error(char *str, int shouldFree, int closeFile);
-void add(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void divs(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
-void mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-void stack(stack_t **stack, unsigned int line_number);
-void queue(stack_t **stack, unsigned int line_number);
 
 #endif /*_MONTY_H_*/
