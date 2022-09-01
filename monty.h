362500a1b21c98776a0f4dbf6b13dcbbbed7dbcb
m1994d <marlondiaz2994@gmail.com>
2022-09-01 13:56:27 -0500

    monty

--- monty.h
+++ monty.h
@@ -8,7 +8,10 @@
 #include <ctype.h>
 #include <math.h>
 
-
+/**
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
 
 typedef struct stack_s
 {
@@ -17,7 +20,8 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-
+/**
+ */
 
 typedef struct instruction_s
 {
@@ -25,7 +29,8 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
+/**
+ */
 struct pack
 {
 	char *cmd;
@@ -35,10 +40,26 @@ struct pack
 } pack;
 
 void freeStack(stack_t **stack);
-
+int isNumber(char *str);
+char *str_concat(char *s1, char *s2);
 void built_in(stack_t **head);
 void push(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void error(char *str, int shouldFree, int closeFile);
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void divs(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void queue(stack_t **stack, unsigned int line_number);
 
 #endif /*_MONTY_H_*/
