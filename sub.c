c5afa6ee1ac68978eaeee41d38218cb5ec111f09
m1994d <marlondiaz2994@gmail.com>
2022-09-01 22:40:49 -0500

    fix

---
+++ sub.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**/
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	if (current && current->next)
+	{
+		current->n *= -1;
+		add(stack, line_number);
+	}
+	else
+	{
+		freeStack(stack);
+		free(pack.cmd);
+		dprintf(2, "L%d: ", line_number);
+		error("");
