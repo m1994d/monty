91db6bcf149051e1725d5590038fb3f9ad5c03d4
m1994d <marlondiaz2994@gmail.com>
2022-09-02 22:32:28 -0500

    monty

--- monty.h
+++ monty.h
@@ -9,7 +9,12 @@
 #include <math.h>
 
 /**
- * Description: (DLL) doubly linked list node structure
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
 
@@ -21,6 +26,12 @@ typedef struct stack_s
 } stack_t;
 
 /**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
  */
 
 typedef struct instruction_s
@@ -30,6 +41,14 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
+ * struct pack - transport getline and line_number
+ * @cmd: current line in operation
+ * @n: current line number
+ * @fdcode: to close globally the opened file
+ * @mode: 0 stack, 1 queue
+ *
+ * Description: whole line in manipulation and his
+ * position
  */
 struct pack
 {
@@ -50,6 +69,16 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void error(char *str, int shouldFree, int closeFile);
-
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void divs(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void queue(stack_t **stack, unsigned int line_number);
 
 #endif /*_MONTY_H_*/
