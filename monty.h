e70f714c87b67990f2b8f8fb9fa9915f3656459f
Marlon Andrés Diaz Suarez <98677728+m1994d@users.noreply.github.com>
2022-09-03 16:30:06 -0500

    Update monty.h

--- monty.h
+++ monty.h
@@ -65,5 +65,4 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
-void div(stack_t **stack, unsigned int line_number);
 #endif /*_MONTY_H_*/
